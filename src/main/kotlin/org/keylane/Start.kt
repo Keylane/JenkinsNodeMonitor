5addc3e433be416da7a4be2afe71487cd9264986
Andreas Precht <andreas.precht.poulsen@keylane.com>
2018-05-08 19:24:03 +0200

    Its working

--- src/main/kotlin/org/keylane/Start.kt
+++ src/main/kotlin/org/keylane/Start.kt
@@ -3,20 +3,27 @@ package org.keylane
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.html.*
-import io.ktor.http.HttpStatusCode
+import io.ktor.response.respondText
 import io.ktor.routing.*
 import kotlinx.html.*
 
 fun Application.main() {
     install(DefaultHeaders)
-    install(CallLogging)
+//    install(CallLogging)
     install(Routing) {
         get("/") {
-            call.respondHtml(HttpStatusCode.NotFound) {
+            call.respondHtml {
+                head {
+                    title { +"Header Title" }
+                }
                 body {
+                    p { +"Hello world" }
                     h1 { +"Sample application with HTML builders" }
                 }
             }
+        }
+        get("/test") {
+            call.respondText("Hello world")
         }
     }
 }
